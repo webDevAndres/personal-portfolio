6c4749c193ee248ebce6570c202ea7784da55ee0
Andres Macias <macias.andres02@gmail.com>
2022-07-14 08:02:58 -0500

    corrected comment

--- footer-component.js
+++ footer-component.js
@@ -1,8 +1,8 @@
 /*
-Title: nav-component.js
+Title: footer-component.js
 Author: Andres Macias
-Date: 6/25/2022
-Description: cart component for index.html
+Date: 07/14/2022
+Description: footer component
 */
 
 class FooterComponent extends HTMLElement {
@@ -10,11 +10,11 @@ class FooterComponent extends HTMLElement {
         super();
     }
 
-    connectedCallback() {
+    connectedCallback() {  
         this.innerHTML = `<footer>
-        <a href="https://github.com/webDevAndres?tab=repositories">My GitHub Repo</a>
+        <a href="https://github.com/webDevAndres?tab=repositories" target="_blank">My GitHub Repo</a>
         <a href="https://www.bellevue.edu/degrees/bachelor/web-development-bs/" target="_blank">Bellevue Web Development Degree</a>
-        <a href="https://www.youtube.com/user/bellevueuniversity">Bellevue University Youtube</a>
+        <a href="https://www.youtube.com/user/bellevueuniversity" target="_blank">Bellevue University Youtube</a>
         <a href="https://github.com/buwebdev" target="_blank">Bellevue University GitHub</a>
     </footer>`;
     }
