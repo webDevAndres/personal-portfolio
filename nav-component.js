99f3fe5168970d08899f923930f93e612bdd3de1
webDevAndres <macias.andres02@gmail.com>
2022-10-30 20:40:44 -0500

    testing links

--- nav-component.js
+++ nav-component.js
@@ -12,14 +12,14 @@ class NavComponent extends HTMLElement{
     connectedCallback() {
         this.innerHTML = `<nav class="desktop-nav" id="myNav">
         <a class="full-width" href="./index.html">Andres Macias</a>
-        <a href="/index.html">Home</a>
-        <a href="/about.html">About</a>
-        <a href="/resume.html">Resume</a>
-        <a href="/projects.html">Projects</a>
-        <a href="/databases.html">Database Diagrams</a>
-        <a href="/api-unit-tests.html">Api Unit Tests</a>
-        <a href="/web-430/macias-tech-value-stream.html">DevOps</a>
-        <a href="/bucket-list.html">Bucket List</a>
+        <a href="index.html">Home</a>
+        <a href="about.html">About</a>
+        <a href="resume.html">Resume</a>
+        <a href="projects.html">Projects</a>
+        <a href="databases.html">Database Diagrams</a>
+        <a href="api-unit-tests.html">Api Unit Tests</a>
+        <a href="web-430/macias-tech-value-stream.html">DevOps</a>
+        <a href="bucket-list.html">Bucket List</a>
         <a href="javascript:void(0);" id="addResponsiveClass" class="icon"><i class="fas fa-bars" id="openClose"></i></a>
     </nav>`;
     }
